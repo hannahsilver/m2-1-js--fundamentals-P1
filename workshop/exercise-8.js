793db0d8c43cf9d98735a4c1c7c0d21d78f62041
Hannah Silver <hannah_silver@hotmail.com>
2020-06-15 16:56:34 -0400

    exercise 8

--- workshop/exercise-8.js
+++ workshop/exercise-8.js
@@ -3,3 +3,12 @@
 // between 0 and 5000
 
 // ANSWER: the number you should see in the console is 3127500
+
+let sum = 0;
+
+for (let x = 0; x <= 5000; x++) {
+    if (x % 4 === 0) {
+        sum += x;
+    } 
+}  
+    console.log(sum);
